f53b81fd26baf88977fc54c49f72bb79d3b26dcd
Joseph Kanichai <kjosephsubash@gmail.com>
2024-04-11 00:11:04 +0200

    frontend: initial commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import path from "path";
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
+})
