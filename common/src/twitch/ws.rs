53e865f7fc26a59cd05d73b879f78f94ea99060e
Klaus Eckelt <klaus@eckelt.info>
2026-08-17 14:56:52 +0200

    Silence warnings for known unimplemented ws message types

--- common/src/twitch/ws.rs
+++ common/src/twitch/ws.rs
@@ -497,7 +497,13 @@ async fn ws_reader(
                     }
                     _ => warn!("Unknown response {:#?}", r),
                 },
-                Err(err) => warn!("Failed to parse ws message {:#?} \nmessage {m}", err),
+                Err(err) => {
+                    if m.contains("\"type\":\"points-spent\"") || m.contains("\"type\":\"active-multipliers-updated\"") {
+                        trace!("Unimplemented ws message type: {m}");
+                    } else {
+                        warn!("Failed to parse ws message {:#?} \nmessage {m}", err);
+                    }
+                }
             }
         }
     }
